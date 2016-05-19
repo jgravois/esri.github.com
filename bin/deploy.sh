47eeef7d33aff757166681c99fa5c2d5a7218212
john gravois <jgravois@esri.com>
2016-05-18 17:24:09 -0700

    making progress

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
-
-npm run build
-
-git checkout master
-
-# create a local gh-pages branch containing the splitted output folder
-git subtree split --prefix built -b gh-pages --force
-
-# force the push of the gh-pages branch to the remote gh-pages branch at origin
-git push origin gh-pages --force
-
-# delete the local gh-pages because you will need it: ref
 git branch -D gh-pages
-
-git checkout master
+git checkout -b gh-pages
+npm run build
+git add -f built/
+git commit -m "deploy"
+git push origin `git subtree split --prefix built gh-pages`:gh-pages --force
+git reset --hard HEAD~1
+git checkout v2
+git branch -D gh-pages
